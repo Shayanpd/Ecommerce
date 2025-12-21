8795ef3d27aece96f8ae7d6dbc809f9fad0e9416
Shayan <shayanpd@kth.se>
2025-12-21 22:22:00 +0100

    ecommerce

---
+++ app/admin/page.tsx
@@ -0,0 +1,32 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function AdminDashboard() {
+    return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <DashboardCard title="Sales" subtitle="Test" body="body"/>
+    </div>)
+}
+
+function getSalesData(){
+    
+}
+
+type DashboardCardProps = {
+    title: string
+    subtitle: string
+    body: string
+}
+
+function DashboardCard({title, subtitle, body}:DashboardCardProps){
+    return         <Card>
+            <CardHeader>
+                <CardTitle>
+                    {title}
+                </CardTitle>
+            </CardHeader>
+            <CardDescription>{subtitle}</CardDescription>
+            <CardContent>
+                <p>{body}</p>
+            </CardContent>
+        </Card>
+}
